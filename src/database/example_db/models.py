c753f36540a4fb52ee8d7da1c1538aaf9b4c9f75
Yueran <taeyoulee@gmail.com>
2014-06-14 12:54:40 +0800

    Customer, Order, OrderItem, Menu, MenuItem, FoodStall, Bill added into models.py

--- src/database/example_db/models.py
+++ src/database/example_db/models.py
@@ -14,3 +14,33 @@ class Person(models.Model): # Inherits from django's Model class
 class FacebookUser(models.Model):
     email = models.EmailField()
     profile_img = models.ImageField(upload_to="/path/to/media/uploads/")
+
+
+class Customer(Person):
+    customerID=models.AutoField(primary_key=True)
+    phone_number=models.IntegerField(max_length=8)
+
+class Order(models.Model):
+    orderID= models.ForeignKey('Customer')
+    quantity= models.IntegerField(max_length=100)
+    date_created=models.DateTimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+    order_itemID=models.ForeignKey('OrderItem')
+    order_item_name=models.ForeignObject('MenuItem')
+
+class Menu(models.Model):
+    menuID=models.ForeignKey('FoodStall')
+    #menuItems=models.ForeignObject()
+
+class MenuItem(models.Model):
+    menuItemID=models.ForeignKey('Menu')
+    menuItemName=models.CharField(max_length=30)
+
+class FoodStall(models.Model):
+    #restaurantID ＝models.AutoField(primary_key=True)
+    restaurantName=models.CharField()
+    restaurantAddr=models.CharField()
+
+class Bill(models.Model):
+    billID= models.ForeignKey('Customer')
